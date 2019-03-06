37e6ffc9512c904c8c692edeaf80ee4fbdf97458
namja14 <tjdals9503@gmail.com>
2019-03-06 22:25:23 +0900

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn roadcatproject.wsgi --log-file -
--- roadcatproject/settings.py
+++ roadcatproject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2clt8*r97+pb99_v!3t-t4q^*4^&2nwddua2b-649+l$9vp=tx'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','2clt8*r97+pb99_v!3t-t4q^*4^&2nwddua2b-649+l$9vp=tx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG',True))
 
 ALLOWED_HOSTS = []
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -151,3 +152,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #media 파일들이 어디로 모일것인지를 쓰는 곳   
 
 MEDIA_URL = '/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
